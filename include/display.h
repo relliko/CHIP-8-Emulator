8412adc5d84ffb6de4448ef35b31625196c7acce
Relliko <apolywka@gmail.com>
2021-09-16 02:36:25 -0400

    implemented drawing and rendering of pixels

--- include/display.h
+++ include/display.h
@@ -1,7 +1,7 @@
 #include <SDL2/SDL.h>
 
 // Display is 64x32 px or 128x64 for SUPER-CHIP
-#define WINDOW_SCALAR 10
+#define WINDOW_SCALAR 15
 #define SCREEN_WIDTH (64*WINDOW_SCALAR) // pixels
 #define SCREEN_HEIGHT (32*WINDOW_SCALAR)
 
--- src/display.c
+++ src/display.c
@@ -1,14 +1,16 @@
 #include <display.h>
+#include <unistd.h>
 
 SDL_Window *window;
+SDL_Renderer* renderer = NULL;
 
 // Makes up the bits representing the pixels on screen
 // CHIP-8 is monochrome so bits are sufficient to represent them.
-uint8_t pixels[SCREEN_HEIGHT/8][SCREEN_WIDTH/8];
+uint8_t pixels[(SCREEN_HEIGHT/WINDOW_SCALAR)/8][(SCREEN_WIDTH/WINDOW_SCALAR)/8];
 
 void init_display(void) {
     SDL_Init(SDL_INIT_VIDEO);
-    printf("%lu\n", sizeof(pixels[0]));
+    // Initialize the window
     window = SDL_CreateWindow(
         "CHIP-8 EMULATOR",                 // window title
         SDL_WINDOWPOS_UNDEFINED,           // initial x position
@@ -17,9 +19,15 @@ void init_display(void) {
         SCREEN_HEIGHT,                     // height, in pixels
         SDL_WINDOW_OPENGL                  // flags - see below
     );
-
     if (window == NULL) {
         printf("Could not create window: %s\n", SDL_GetError());
+        exit(1);
+    }
+
+    // Initialize the renderer
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL) {
+        printf("Could not create renderer: %s\n", SDL_GetError());
         exit(1);
     }
 }
@@ -29,6 +37,36 @@ void cleanup_display(void) {
     SDL_Quit();
 }
 
-void render() {
+// Draws a grid on the screen for debugging
+static void draw_pixel_grid(int x, int y) {
+    SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xff);
+    SDL_Rect outlineRect = {x*WINDOW_SCALAR, y*WINDOW_SCALAR, WINDOW_SCALAR, WINDOW_SCALAR};
+    SDL_RenderDrawRect(renderer, &outlineRect);
+}
+
+// Sets the render color to purple
+static void set_color_purple() {
+    SDL_SetRenderDrawColor(renderer, 0x3c, 0x00, 0x5a, 0xff);
+}
+
+// Draws the pixel at x,y as a purple rect
+static void draw_purple_pixel(int x, int y) {
+    set_color_purple();
+    SDL_Rect fillRect = {x*WINDOW_SCALAR, y*WINDOW_SCALAR, WINDOW_SCALAR, WINDOW_SCALAR};
+    SDL_RenderFillRect(renderer, &fillRect);
+}
+
+void render(void) {
+    SDL_SetRenderDrawColor(renderer, 0x3c, 0x00, 0x5a, 0xff);
+    // Draw a rect for every pixel in the grid
+    // offset position based on x,y of pixels array
+    for (int y = 0; y < SCREEN_HEIGHT/WINDOW_SCALAR; y++) {
+        for (int x = 0; x < SCREEN_WIDTH/WINDOW_SCALAR; x++) {
+            draw_purple_pixel(x, y);
+            draw_pixel_grid(x, y);
+        }
+    }
+    SDL_RenderPresent(renderer);
+
     return;
 }
