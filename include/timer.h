841722d983544323fdea4707efe4280d805bc699
Relliko <apolywka@gmail.com>
2021-09-17 00:12:12 -0400

    threw out method of implementing timer. going to tie it to cpu hz instead.

--- include/timer.h
+++ include/timer.h
@@ -1,13 +1,10 @@
 #include <stdint.h>
-#include <sys/time.h>
+
+#define TIMER 
 
 uint8_t delay_timer;
 uint8_t sound_timer;
 
-// struct timeval {
-//     long tv_sec;
-//     long tv_usec;
-// }
 
 void update_timers(void);
 void init_timers(void);
--- src/timer.c
+++ src/timer.c
@@ -4,13 +4,9 @@
 uint8_t delay_timer = 0;
 uint8_t sound_timer = 0;
 
-struct timeval prev;
-struct timeval now;
-
 int test = 0;
 
 void init_timers(void) {
-    gettimeofday(&prev, NULL);
 }
 
 static void decrement_8bit_timer(uint8_t* t) {
@@ -22,15 +18,13 @@ static void decrement_8bit_timer(uint8_t* t) {
 
 // Decrements both bit timers 60 times per second 
 void update_timers(void) {
-    gettimeofday(&now, NULL);
     // printf("%ld\n", now.tv_usec);
     // printf("%ld\n", prev.tv_usec);
-    double ms = (((now.tv_sec * 1000000) + now.tv_usec) - ((prev.tv_sec * 1000000) + prev.tv_usec)) / (double) 1000;
-    if (ms < 16.6666) {
-        //printf("%f\n", ms);
-        // Skip execution
-        return;
-    } 
+    // if (ms < 1.6666) {
+    //     //printf("%f\n", ms);
+    //     // Skip execution
+    //     return;
+    // } 
     if (test >= 60) {
         printf("one second\n");
         test = 0;
@@ -40,5 +34,4 @@ void update_timers(void) {
     decrement_8bit_timer(&delay_timer);
     decrement_8bit_timer(&sound_timer);
     // Store the last time the timers got changed.
-    gettimeofday(&prev, NULL);
 }
